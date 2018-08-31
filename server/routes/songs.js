f4d55e13eef7e2e4aa3b526188377bc2f5aa770a
CaitlinCodes <caiit@me.com>
2018-08-31 14:55:04 -0600

    song route

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -1,26 +1,57 @@
 let router = require('express').Router()
-let Playlists = require('../models/playlists')
 let Songs = require('../models/songs')
 
 
-router.post('/login', (req, res, next) => {
-  Playlists.findOne({
-    username: req.body.username,
-    pin: req.body.pin
-  }).then(user => {
-    if (user) {
-      return res.send(user)
-    }
-    return res.status(401).send({
-      error: "Invalid Login"
-    })
-  })
+router.get('/', (req, res, next) => {
+  Songs.find({})
+    .then(plist => {
+      res.send(plist)
+    }).catch(next)
 })
 
-router.post('/register', (req, res, next) => {
-  Playlists.create(req.body).then(user => {
-    res.send(user)
-  }).catch(next)
+router.post('/', (req, res, next) => {
+  Songs.create(req.body)
+    .then(post => {
+      res.send(post)
+    }).catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  Songs.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.send({
+      message: 'Success'
+    })).catch(next)
+})
+
+// router.post('/:id/upvote', (req, res, next) => {
+//   Songs.findById(req.params.id)
+//     .then((post) => {
+//       post.vote++
+//       return post.save()
+//     })
+//     .then(() => res.send({
+//       message: "You liked this song."
+//     }))
+//     .catch(next)
+// })
+
+// router.post('/:id/downvote', (req, res, next) => {
+//   Songs.findById(req.params.id)
+//     .then((post) => {
+//       post.vote--
+//       return post.save()
+//     })
+//     .then(() => res.send({
+//       message: "You did not like this song."
+//     }))
+//     .catch(next)
+// })
+
+router.delete('/:id', (req, res, next) => {
+  Songs.findByIdAndRemove(req.params.id)
+    .then(() => res.send({
+      message: 'Deleted'
+    })).catch(next)
 })
 
 module.exports = router

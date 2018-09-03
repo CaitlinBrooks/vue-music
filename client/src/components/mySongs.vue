aa4fcc103fd21e50aa7b2f610bb6fe2a3da62568
CaitlinCodes <caiit@me.com>
2018-09-03 16:18:17 -0600

    mySongs

---
+++ client/src/components/mySongs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mySongs">
+    <h1>MySongs</h1>
+    <div v-for="song in playlist">
+      <button type="button" @click="deleteSong(song)">Delete Song</button>
+      <audio controls>
+        <source :src="song.preview">
+      </audio>
+      <img :src="song.coverArt" alt="coverArt">
+      <p>Title: {{song.title}}</p>
+      <p>Artist: {{song.artist}}</p>
+      <p>Price: {{song.price}}</p>
+      <p>Album: {{song.album}}</p>
+    </div>
+  </div>
+</template>

7ed57d565dacd1dbf6a8a3f730cba2d41e7fe78f
CaitlinCodes <caiit@me.com>
2018-09-03 16:08:48 -0600

    song

--- client/src/models/song.js
+++ client/src/models/song.js
@@ -1,6 +1,6 @@
 export default class Song {
   constructor(song) {
-    this.coverArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+    this.coverArt = song.artworkUrl60.replace(/60x60/g, "150x150")
     this.artist = song.artistName
     this.album = song.collectionName
     this.title = song.trackName

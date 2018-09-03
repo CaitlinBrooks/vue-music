eb7c188b530686e6d1cb7c5f50c973531c51b5ba
CaitlinCodes <caiit@me.com>
2018-09-03 16:54:38 -0600

    results component for songs coming back

---
+++ client/src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="results">
+    <h1>Song Search Results</h1>
+    <div v-for="song in songResults">
+      <button type="button" @click="addSong(song)">Add a Song</button>
+      <!-- <audio controls>
+        <source :src="song.preview">
+      </audio> -->
+      <img :src="song.coverArt" alt="coverArt">
+      <p>Title: {{song.title}}</p>
+      <p>Artist: {{song.artist}}</p>
+      <p>Price: {{song.price}}</p>
+      <p>Album: {{song.album}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'results',
+    data() {
+      return {}
+    },
+    mounted() {
+      this.$store.dispatch('getSongs')
+    },
+    methods: {
+      deleteSong(song) {
+        this.$store.dispatch('deleteSong', song)
+      }
+    },
+    computed: {
+      playlist() {
+        return this.$store.state.playlist
+      }
+    },
+    components: {}
+  }
+</script>
